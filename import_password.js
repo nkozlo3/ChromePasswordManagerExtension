169f732420031a92a1818dee3d66e8b685b4a515
nkozlo3 <80439868+nkozlo3@users.noreply.github.com>
2022-12-08 14:12:37 -0600

    Started creating master password and encryptions

--- import_password.js
+++ import_password.js
@@ -1,10 +1,5 @@
 import getPassword from "./password.js";
 
-// call getPassword and log the result
-// call getPassword and make it the header of the page
-//<button id="getPasswordButton">Get Password</button>
-// if the above button from password.html is clicked, call getPassword
-// and make it the header of the page
 document.getElementById("getPasswordButton").addEventListener("click", () => {
   document.getElementById("password").innerHTML = getPassword();
 });
